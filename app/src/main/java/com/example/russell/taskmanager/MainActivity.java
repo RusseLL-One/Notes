e5d896bc437e332a18b99271f22e20642ef2e1d8
RusseLL-One <rassel2121@gmail.com>
2018-05-03 00:06:56 +0700

    init commit

---
+++ app/src/main/java/com/example/russell/taskmanager/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.russell.taskmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listview);
+
+    }
+
+    public void onClick(View view) {
+    }
+}
